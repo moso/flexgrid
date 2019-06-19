6a9cd121ebf2ec608d13313da2e185e0d9aaf7a6
Morten Sørensen <morten@moso.io>
2019-06-19 16:37:42 +0200

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssoWebpackPlugin = require('csso-webpack-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const FriendlyErrors = require('friendly-errors-webpack-plugin');
+
+module.exports = {
+    stats: {
+        children: false
+    },
+
+    entry: {
+        flexgrid: './src/flexgrid.scss'
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+    },
+
+    mode: 'none',
+
+    devtool: 'source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new FriendlyErrors(),
+        new FixStyleOnlyEntriesPlugin(),
+        new MiniCssExtractPlugin('[name].css'),
+        new CssoWebpackPlugin({
+            pluginOutputPostfix: 'min'
+        })
+    ]
+}
